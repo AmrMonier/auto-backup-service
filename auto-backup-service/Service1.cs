0685538ff09c56a0967a060e997dbce8a619d798
Amr Monier <iam@amrmonier.com>
2023-10-31 08:03:11 +0200

    added taking restore point trigger

--- auto-backup-service/Service1.cs
+++ auto-backup-service/Service1.cs
@@ -6,9 +6,10 @@
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
-    using System.ServiceProcess;
+using System.ServiceProcess;
     using System.Text;
     using System.Threading.Tasks;
+using System.Management;
 
     namespace auto_backup_service
     {
@@ -26,15 +27,28 @@
                 File.AppendAllText(logFilePath, "System Shutdown at " + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + Environment.NewLine);
             }
             protected override void OnStart(string[] args)
-
             {
-            
-                File.AppendAllText(logFilePath, "Service Started at " + DateTime.Now.ToString("dd-mm-yyyy HH:mm:ss")+ Environment.NewLine);
+            //string strCmdText;
+           // strCmdText = "Checkpoint-Computer -Description '" + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + "'";
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            ManagementScope oScope = new ManagementScope("\\\\localhost\\root\\default");
+            ManagementPath oPath = new ManagementPath("SystemRestore");
+            ObjectGetOptions oGetOp = new ObjectGetOptions();
+            ManagementClass oProcess = new ManagementClass(oScope, oPath, oGetOp);
+            ManagementBaseObject oInParams = oProcess.GetMethodParameters("CreateRestorePoint");
+            oInParams["Description"] = "New Check point";
+            oInParams["RestorePointType"] = 12; // MODIFY_SETTINGS
+            oInParams["EventType"] = 100;
+
+            ManagementBaseObject oOutParams = oProcess.InvokeMethod("CreateRestorePoint", oInParams, null);
+         //   Process.Start(strCmdText);
+
+            File.AppendAllText(logFilePath, "Service Started at [" + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + "]"+ Environment.NewLine);
             }
 
             protected override void OnStop()
             {
-                File.AppendAllText(logFilePath,"[" + DateTime.Now.ToString("dd-mm-yyyy HH:mm:ss") + "]" + "Service Stoped " +  Environment.NewLine);
+            File.AppendAllText(logFilePath,"[" + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + "] " + "Service Stoped " +  Environment.NewLine);
               //  File.AppendAllText(logFilePath, "Service Stoped at " + DateTime.Now.ToString("dd-mm-yyyy HH:mm:ss") + Environment.NewLine);
             }
 
@@ -42,7 +56,6 @@
             {
                 File.AppendAllText("D:\\shutdown-logs.txt", "Shutted Down" + Environment.NewLine);
                 File.AppendAllText(logFilePath, "System Shutdown at " + DateTime.Now.ToString("dd-mm-yyyy HH:mm:ss") + Environment.NewLine);
-
                 base.OnShutdown();
             }
         }
